61454d263e951847084f772d31dc05386f4110b9
coling01 <colin.greenwood@hmcts.net>
2025-10-29 16:54:20 +0000

    chore: get rid of z in authz packages

--- demo-project/src/main/java/uk/gov/hmcts/controllers/IdentityControllerForDemo.java
+++ demo-project/src/main/java/uk/gov/hmcts/controllers/IdentityControllerForDemo.java
@@ -4,10 +4,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import uk.gov.moj.cpp.auth.http.dto.LoggedInUserPermissionsResponse;
-import uk.gov.moj.cpp.auth.http.dto.UserGroup;
+import uk.gov.moj.cpp.authz.http.dto.LoggedInUserPermissionsResponse;
+import uk.gov.moj.cpp.authz.http.dto.UserGroup;
 
 import java.util.List;
 
